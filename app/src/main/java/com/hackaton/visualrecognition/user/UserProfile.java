8202b7c4cf4db1fa69763f315bfb927d1a58177d
fatih.erol <yufae@yahoo.com>
2017-10-26 15:46:06 +0300

    Profile Activity Continue

---
+++ app/src/main/java/com/hackaton/visualrecognition/user/UserProfile.java
@@ -0,0 +1,15 @@
+package com.hackaton.visualrecognition.user;
+
+import java.util.List;
+
+/**
+ * Created by fatih.erol on 26.10.2017.
+ */
+
+public class UserProfile {
+
+    public List<String> getCrossAllergens(){
+
+        return null;
+    }
+}
