471fb51dab93a8106badf6b251bcf66f5ed5e7b4
[Ünver Can Ünlü] <[unvercanunlu@gmail.com]>
2017-10-26 23:21:21 +0300

    context is updated

--- app/src/main/java/com/hackaton/chatbot/ChatBot.java
+++ app/src/main/java/com/hackaton/chatbot/ChatBot.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.hackaton.chatbot.activity.ChatActivity;
 import com.ibm.watson.developer_cloud.conversation.v1.Conversation;
 import com.ibm.watson.developer_cloud.conversation.v1.model.Context;
+import com.ibm.watson.developer_cloud.conversation.v1.model.GetDialogNodeOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -34,16 +35,23 @@ public class ChatBot {
         else
             return instance;
     }
-    
+
     public List<String> sendMessage(String inputMessage) {
         List<String> responses = new ArrayList<String>();
 
         InputData.Builder input = new InputData.Builder(inputMessage);
         MessageOptions.Builder optionsBuilder = new MessageOptions.Builder(WORKSPACE).input(input.build());
-        if (context != null)
+        if (context != null) {
+            context.put("my_list", new String[]{"avocado"});
             optionsBuilder.context(context);
+        }
+
         MessageOptions options = optionsBuilder.build();
+        Log.i("CHATBOT", "Message: " + options.toString());
         MessageResponse response = this.service.message(options).execute();
+        Log.i("CHATBOT", "Response: " + response.toString());
+        if (context != null)
+            Log.i("CHATBOT", "Context: " + context.toString());
         List<RuntimeIntent> intents = response.getIntents();
         for (RuntimeIntent intent : intents) {
             String intentName = intent.getIntent();
@@ -55,6 +63,7 @@ public class ChatBot {
         }
         if (context == null)
             context = response.getContext();
+
         if (!ChatActivity.intentCaught) {
             List<String> responseMessages = response.getOutput().getText();
             for (String responseString : responseMessages)
